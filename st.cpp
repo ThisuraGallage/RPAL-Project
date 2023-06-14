26ed960f677d397e07d77b1ba74429fdf5078c8d
ThemiraChathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2023-06-14 13:05:18 +0530

    updated st creation

--- st.cpp
+++ st.cpp
@@ -4,22 +4,39 @@ Node *standerdize_let(Node *node)
 {
     if (node == nullptr)
         return node;
+    node->value = "gamma";
+    node->left->value = "lambda";
+    Node *temp = node->left->left->right;
+    node->left->left->right = node->left->right;
+    node->left->right = temp;
+    return node;
+}
+
+// Function to perform post-order traversal
+void postOrderTraversal(Node *node)
+{
+    if (node == nullptr)
+        return;
+
+    // Traverse left subtree
+    postOrderTraversal(node->left);
+
+    // Traverse right subtree
+    postOrderTraversal(node->right);
+
+    // Process current node
     if (node->value == "let")
     {
-        node->value = "gamma";
-        node->left->value = "lambda";
-        Node *temp = node->left->left->right;
-        node->left->left->right = node->left->right;
-        node->left->right = temp;
-        standerdize_let(node->left->left->right);
-        standerdize_let(node->left->right);
+        node = standerdize_let(node);
     }
-    return node;
+
+    // You can perform additional operations on the current node if needed
+    // based on its TokenType or other properties.
 }
 
 Node *standerdize(Node *node)
 {
-    node = standerdize_let(node);
+    postOrderTraversal(node);
     return node;
 }
 
